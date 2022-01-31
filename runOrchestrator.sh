e12a6523a2da1e9d0dc3826080ee1af56628681a
dsuneja <96498261+dsuneja@users.noreply.github.com>
2022-01-31 11:13:46 -0500

    argument of branch added for analyzer code

--- runOrchestrator.sh
+++ runOrchestrator.sh
@@ -1,6 +1,13 @@
 curl -L https://github.com/mulesoft/mule-migration-assistant/archive/refs/heads/chore/mig-cost.zip -o mig-cost.zip | tee -a run.log
 unzip mig-cost.zip | tee -a run.log
-curl -L https://github.com/dsuneja/mule3Migrator/archive/refs/heads/development.zip -o mule3MigratorOrchestrator.zip | tee -a run.log
+if  [ $# -ne 0 ] && [  'development' == $1 ]
+then
+	curl -L https://github.com/dsuneja/mule3Migrator/archive/refs/heads/development.zip -o mule3MigratorOrchestrator.zip | tee -a run.log
+	echo 'in if'
+else
+	curl -L https://github.com/dsuneja/mule3Migrator/archive/refs/heads/main.zip -o mule3MigratorOrchestrator.zip | tee -a run.log
+	echo 'in else'
+fi
 unzip mule3MigratorOrchestrator.zip | tee -a run.log
 cp mule3Migrator-development/mma-parent-pom.xml mule-migration-assistant-chore-mig-cost/ | tee -a run.log
 cp -R mule3Migrator-development/mule-migrator-orchestrator mule-migration-assistant-chore-mig-cost | tee -a run.log
